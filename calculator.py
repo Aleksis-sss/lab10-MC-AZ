a33be8b78faffce8e6aa935e98102c0dbd5a25ab
Aleksis-sss <alex.zeyts@gmail.com>
2025-04-09 10:24:24 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,8 +4,3 @@ calculator.py
 
 One function per operation, in order.
 """
-
-
-git add .	     # staging all files to be saved
-git commit -m "modified calculator p1" # saving changes w/ message
-git push	
